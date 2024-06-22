21f1685f64f4f8ab70a3d238fdef1a8761ec3920
LohitRamachandra <tripurana.lohit@gmail.com>
2024-06-22 23:58:49 +0530

    Initial

---
+++ studentsadminportal-ui/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './students/students/students.component';
+
+const routes: Routes = [
+  {
+    path: '', component: StudentsComponent
+  },
+  {
+    path: 'students', component: StudentsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ studentsadminportal-ui/src/app/students/students/students.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../student.service';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+
+  constructor(private studentService: StudentService){
+
+  }
+  ngOnInit(): void {
+    //Fetch Students
+    this.studentService.getStudent()
+    .subscribe(
+      (successResponse) =>{
+        console.log(successResponse);
+      },
+      (errorResponse) => {
+        console.log(errorResponse);
+      }
+    );
+  }
+
+}
